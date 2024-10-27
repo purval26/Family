115a131f163875f625d00ff4fa46f438939e2cbf
purval26 <purvalradadiya@gmail.com>
2024-10-27 21:25:56 +0530

    js

--- index.js
+++ index.js
@@ -1,39 +1,43 @@
 var urls = [
-    './20241027_194842.jpg',
-    './20241027_194849.jpg',
-    './20241027_194900.jpg',
-    './20241027_194911.jpg',
-    './20241027_195145.jpg',
-    './20241027_195156.jpg',
-    './20241027_195453.jpg',
-    './20241027_195502.jpg',
-    './20241027_195513.jpg',
-    './20241027_195602.jpg',
-    './20241027_195608.jpg',
-    './20241027_195615.jpg', 
-    ];
-    
-    function downloadAll() {
-    
-    
+  './20241027_194842.jpg',
+  './20241027_194849.jpg',
+  './20241027_194900.jpg',
+  './20241027_194911.jpg',
+  './20241027_195145.jpg',
+  './20241027_195156.jpg',
+  './20241027_195453.jpg',
+  './20241027_195502.jpg',
+  './20241027_195513.jpg',
+  './20241027_195602.jpg',
+  './20241027_195608.jpg',
+  './20241027_195615.jpg', 
+];
+
+function downloadAll() {
+  // Ask for user confirmation
+  var userConfirmed = confirm("Do you want to download " + urls.length + " files?");
+  if (userConfirmed) {
       for (var i = 0; i < urls.length; i++) {
-        forceDownload(urls[i], urls[i].substring(urls[i].lastIndexOf('/')+1,urls[i].length))
+          forceDownload(urls[i], urls[i].substring(urls[i].lastIndexOf('/') + 1, urls[i].length));
       }
-    }
-    function forceDownload(url, fileName){
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.responseType = "blob";
-        xhr.onload = function(){
-            var urlCreator = window.URL || window.webkitURL;
-            var imageUrl = urlCreator.createObjectURL(this.response);
-            var tag = document.createElement('a');
-            tag.href = imageUrl;
-            tag.download = fileName;
-            document.body.appendChild(tag);
-            tag.click();
-            document.body.removeChild(tag);
-        }
-        xhr.send();
-    }
-    
+  } else {
+      console.log("Download cancelled by the user.");
+  }
+}
+
+function forceDownload(url, fileName) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "blob";
+  xhr.onload = function() {
+      var urlCreator = window.URL || window.webkitURL;
+      var imageUrl = urlCreator.createObjectURL(this.response);
+      var tag = document.createElement('a');
+      tag.href = imageUrl;
+      tag.download = fileName;
+      document.body.appendChild(tag);
+      tag.click();
+      document.body.removeChild(tag);
+  }
+  xhr.send();
+}
