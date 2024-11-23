5b4dfd3593587321805ff8e2cc5fde999d2c16b9
purval26 <purvalradadiya@gmail.com>
2024-11-23 23:50:20 +0530

    add

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-var urls = [
-  './20241027_194842.jpg',
-  './20241027_194849.jpg',
-  './20241027_194900.jpg',
-  './20241027_194911.jpg',
-  './20241027_195145.jpg',
-  './20241027_195156.jpg',
-  './20241027_195453.jpg',
-  './20241027_195502.jpg',
-  './20241027_195513.jpg',
-  './20241027_195602.jpg',
-  './20241027_195608.jpg',
-  './20241027_195615.jpg', 
-];
+var urls = [];
+var files = require('fs').readdirSync('.');
+files.forEach(function(file) {
+  if (file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.gif') || file.endsWith('.mp4')) {
+    urls.push('./' + file);
+  }
+});
 
 function downloadAll() {
   // Ask for user confirmation
